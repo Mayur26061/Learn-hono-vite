caf3a9ff7a00ebfd95ba472506fee6fc77f381f9
Mayurrajsinh Rathod <mdridrdrr@gmail.com>
2025-11-03 00:01:20 +0530

    Added frontend bundle to backend and export route types

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,15 @@
 import { Hono } from 'hono';
 import { logger } from 'hono/logger';
 import expenseRoute from './routes/expense';
+import { serveStatic } from 'hono/bun'
 
 const app = new Hono()
 app.use("*", logger())
-app.get('/', (c) => c.json({ name: 'Hono!' }))
-app.route("/api/expense", expenseRoute)
+// app.get('/', (c) => c.json({ name: 'Hono!' }))
+const apiRoute = app.basePath("/api").route("/expense", expenseRoute)
+
+app.use('*', serveStatic({ root: './frontend/dist' }))
+app.get('*', serveStatic({ path: './frontend/dist/index.html' }))
 
 export default app
+export type ApiRoute = typeof apiRoute;
